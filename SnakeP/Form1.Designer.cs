ea15a3cd01fb4a41a70c3ee7b4a7051672913fa9
niwired31 <niwired31@gmail.com>
2019-07-02 22:08:39 +0200

    Erste Benutzeroberfläche

--- SnakeP/Form1.Designer.cs
+++ SnakeP/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SnakeP
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Erforderliche Designervariable.
@@ -28,13 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonNeuesSpiel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelScore = new System.Windows.Forms.Label();
+            this.labelGrafik = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.labelGrafik, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 52F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(920, 505);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 4;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 138F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 146F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 153F));
+            this.tableLayoutPanel2.Controls.Add(this.label1, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.buttonNeuesSpiel, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.labelScore, 3, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 456);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(914, 46);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // buttonNeuesSpiel
+            // 
+            this.buttonNeuesSpiel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonNeuesSpiel.Font = new System.Drawing.Font("Comic Sans MS", 12F);
+            this.buttonNeuesSpiel.Location = new System.Drawing.Point(3, 3);
+            this.buttonNeuesSpiel.Name = "buttonNeuesSpiel";
+            this.buttonNeuesSpiel.Size = new System.Drawing.Size(132, 40);
+            this.buttonNeuesSpiel.TabIndex = 0;
+            this.buttonNeuesSpiel.Text = "Neues Spiel";
+            this.buttonNeuesSpiel.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(686, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 46);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Score";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // labelScore
+            // 
+            this.labelScore.AutoSize = true;
+            this.labelScore.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelScore.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelScore.Font = new System.Drawing.Font("Comic Sans MS", 16.2F);
+            this.labelScore.Location = new System.Drawing.Point(764, 0);
+            this.labelScore.Name = "labelScore";
+            this.labelScore.Size = new System.Drawing.Size(147, 46);
+            this.labelScore.TabIndex = 4;
+            this.labelScore.Text = "123";
+            this.labelScore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelGrafik
+            // 
+            this.labelGrafik.AutoSize = true;
+            this.labelGrafik.BackColor = System.Drawing.Color.Aquamarine;
+            this.labelGrafik.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelGrafik.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelGrafik.Location = new System.Drawing.Point(3, 0);
+            this.labelGrafik.Name = "labelGrafik";
+            this.labelGrafik.Size = new System.Drawing.Size(914, 453);
+            this.labelGrafik.TabIndex = 1;
+            this.labelGrafik.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(920, 505);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "MainForm";
+            this.Text = "SnakeP";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button buttonNeuesSpiel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelScore;
+        private System.Windows.Forms.Label labelGrafik;
     }
 }
 
--- SnakeP/Program.cs
+++ SnakeP/Program.cs
@@ -16,7 +16,7 @@ namespace SnakeP
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
